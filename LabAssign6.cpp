33fca9d0f4f7a6c0443b8cd351101c848f1ac19d
GABRIEL NELLIS <5089401@nwpolytech.ca>
2024-11-19 22:47:54 -0700

    made progress on the assignment

--- LabAssign6.cpp
+++ LabAssign6.cpp
@@ -9,18 +9,19 @@ using alloc = std::pair<range, int>;
 using freelist_t = std::vector<range>;
 using alloclist_t = std::vector<alloc>;
 #endif LabAssign6
-int size=0; //size of the range
-int start=0; //starting address of the range
+int reqTotal = 0, reqSatis = 0, reqUnsat = 0;
 range allocated; //the range of memory currently allocated from freelist
 int leaseExpirey; //time at which this block will be returned to freelist
 //auto [allocated, leaseExpirey] = a;
 //auto [start, size] = r;
+freelist_t freeList;
+alloclist_t aloccList;
 const int MIN_LEASE = 40;
 const int MAX_LEASE = 70;
 const int MIN_SIZE = 50;
 const int MAX_SIZE = 350;
 const int TIME_LIMIT = 1000;
-const int MEMORY_SIZE = 1000;
+//const int MEMORY_SIZE = 1000;
 const int REQUEST_INTERVAL = 10;
 
 bool comp(const range& a, const range& b) { //for the starting points of objects in freelist
@@ -32,27 +33,44 @@ bool time(const alloc& a, const alloc& b) { //for the times of objects in allocl
 }
 
 void printFree(freelist_t list) {
-
+	for (range e : list) {
+		std::cout << e.second << " size starting at " << e.first << " ";
+	}
 }
 
 void printAlloc(alloclist_t list) {
+	for (alloc e : list) {
+		//printFree(e.first);
 
+	}
 }
 
-void merge(freelist_t list) { //called whenever a request is denied
-
+bool merge() { //called whenever a request is denied
+	bool change = false;
+	for (int i = freeList.size()-1; i > 1; i--) { //going through the free list in reverse order (smallest start point to greatest)
+		if ((freeList[i].first + freeList[i].second) == freeList[i - 1].first) { //if size+start of current==start of current-1
+			freeList[i + 1].first = freeList[i].first; //changing the start to join them
+			freeList[i + 1].second += freeList[i].second; //changing the size to equal the size of both
+			//have to get rid of freeList[i]
+			//can swap freeList[i] with the end, pop, then try to swap it back
+			change = true;
+		}
+	}
+	return change;
 }
 
 void generateReq() {
-	range r = make_pair(rand(), rand());
+	//range r = range((), ()); //first number is the start position, second is the size
+	//alloc a = alloc(r, ()); //second number is the time (in ticks) for it to be allocated for
+	reqTotal++;
+	int size = MIN_SIZE + (rand() % (MAX_SIZE - MIN_SIZE + 1)); //making a number between 50 and 350
+	for (range e : freeList) {
 
+	}
 }
 
 int main() {
-	range r = make_pair(0, MEMORY_SIZE);
-	range p = make_pair(make_pair(20, 100), 45);
-	freelist_t f;
-	int reqTotal = 0, reqSatis = 0, reqUnsat = 0;
+	freeList.push_back(range(0, 1000)); //pushing back all the memory space
 	for (long int clock = 0; clock < TIME_LIMIT; clock++) {
 		if (clock % REQUEST_INTERVAL == 0) { //should I generate a request?
 			generateReq();
